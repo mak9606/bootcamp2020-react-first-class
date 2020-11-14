bf2dddc614ecdb3d802dad4a323ddd09793e114e
mak9606 <alikhan1996116@gmail.com>
2020-11-14 23:15:52 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ function App({name, age}) {
    <div>
      Hello world from react.js
      <br/>
-  <div>Hello again from the other side of the world {name} and age={age-6}</div> 
+  <div>Hello again from the other side of the world  {name} and age={age-6}</div> 
   <Hello fname={name}></Hello>
+  <h4>Unordered list</h4>
+  <ul>
+        <li>one</li>
+    <li>two</li>
+    <li>three</li>
+  </ul>
+  <h4>Ordered list</h4>
+  <ol>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+  </ol>
     </div>
     
   );
--- src/Hello.js
+++ src/Hello.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './proj.css'
 function Hello({fname}){
 return(
-<div className="myname">Hello from another component {fname}</div>
+<div className="myname">Hello from another component, <strong>{fname}</strong></div>
 )
 };
 export default Hello;
